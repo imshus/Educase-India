6cb1a49796b5e3522a4ecfcd0aca5ebfaf05066d
ShudhanshuKumar80 <shudhanshu.bytelogik@gmail.com>
2025-08-26 21:54:15 +0530

    style: home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import Home from './components/Home/home.jsx'
-import Login from './components/Login/Login.jsx'
-import Signup from './components/Signup/Signup.jsx'
-import Profile from './components/Profile/Profile.jsx'
 function App() {
   return (
     <>
      <Home/>
-     <Login/>
-     <Signup/>
-     <Profile/>
     </>
   )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,16 @@
 const Home = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+    return (
+        <div className="flex justify-center items-center w-full h-screen ">
+            <div className="flex flex-col justify-end p-4 w-1/5 min-h-5/6 border border-gray-200 shadow-md">
+                <span className="text-xl font-bold">Welcome to PopX</span>
+                <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span>
+                <div className="">
+                    <button className="bg-blue-500 text-sm text-white mt-4 px-4 py-1 rounded w-full">Create Account</button>
+                    <button className="bg-blue-200 text-sm text-gray-800 mt-2 px-4 py-1 rounded w-full">Already Registered? Login</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 export default Home
 
