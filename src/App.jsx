c012833263124b031be4a35fb46254dfb6270023
ShudhanshuKumar80 <shudhanshu.bytelogik@gmail.com>
2025-08-26 21:23:55 +0530

    structure of main project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from './components/Home/home.jsx'
+import Login from './components/Login/Login.jsx'
+import Signup from './components/Signup/Signup.jsx'
+import Profile from './components/Profile/Profile.jsx'
+function App() {
+  return (
+    <>
+     <Home/>
+     <Login/>
+     <Signup/>
+     <Profile/>
+    </>
+  )
+}
+
+export default App
