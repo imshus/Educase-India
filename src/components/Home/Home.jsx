8664ec395d441a992d045b71e54b0890c8c99f57
ShudhanshuKumar80 <shudhanshu.bytelogik@gmail.com>
2025-08-27 12:07:47 +0530

    style: profile page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,16 @@
 const Home = () => {
     return (
-        <div className="flex justify-center items-center w-full h-screen ">
-            <div className="flex flex-col justify-end p-4 w-1/5 min-h-5/6 border border-gray-200 shadow-md">
+        <div className="flex justify-center items-center h-screen">
+            <div className="flex flex-col justify-end p-4 lg:border lg:border-gray-200 lg:shadow-md lg:w-1/4 min-h-[90%]">
                 <span className="text-xl font-bold">Welcome to PopX</span>
-                <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span>
-                <div className="">
-                    <button className="bg-blue-500 text-sm text-white mt-4 px-4 py-1 rounded w-full">Create Account</button>
-                    <button className="bg-blue-200 text-sm text-gray-800 mt-2 px-4 py-1 rounded w-full">Already Registered? Login</button>
+                <span className="text-sm mt-1">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</span>
+                <div>
+                    <button className="bg-blue-500 text-sm text-white mt-4 px-4 py-1 rounded w-full cursor-pointer">
+                        <p className="text-xs font-semibold">Create Account</p>
+                    </button>
+                    <button className="bg-pink-100 text-gray-800 mt-2 px-4 py-1 rounded w-full lg:mb-4 cursor-pointer">
+                        <p className="text-xs font-semibold">Already Registered? Login</p>
+                    </button>
                 </div>
             </div>
         </div>
